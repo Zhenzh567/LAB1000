06e8007176596297f582c7ac910eb85458f75bc8
Zhenzh567 <zhenzhg@yandex.ru>
2026-06-14 17:03:16 +0200

    Create StotageCell.h

---
+++ StotageCell.h
@@ -0,0 +1,25 @@
+#ifndef PRODUCT_a_INCLUDED
+#define PRODUCT_a_INCLUDED
+
+#include "Product.h"
+#include <vector>
+#include <string>
+
+class StorageCell
+{
+private:
+    int number;
+    double max_size;
+    std::vector<Product> list_of_tovars;
+
+public:
+    StorageCell(int p_number, double p_max_size, std::vector<Product> p_list_of_tovars);
+    int getNumber() const;
+    double getMax_size() const;
+    std::vector<Product> getList_of_tovars() const;  // только const, возвращает копию
+    void addTovar(const Product& product);
+    void deleteTovar(int productId);
+    std::string findTovar(int id2);
+};
+
+#endif
